c57530a7d76783f09112883b11c8b30761015567
BrunoCostaPrado <brunocostaprado@gmail.com>
2022-07-30 03:08:04 -0300

    add

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,9 @@
 export default function Home() {
-  return <div className="">Hello World</div>;
+  return (
+    <div className="h-screen w-screen flex flex-col justify-center">
+      <div className="bg-red-200">First</div>
+      <div className="bg-pink-400">Second</div>
+      
+    </div>
+  );
 }
