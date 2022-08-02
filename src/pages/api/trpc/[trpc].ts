1cd143fe1db5e92b8a49d60a0f356290fe33b429
BrunoCostaPrado <brunocostaprado@gmail.com>
2022-08-02 20:41:47 -0300

    edit

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,7 +1,12 @@
 import { AppRouter } from "@/backend/router";
+import { inferProcedureOutput } from "@trpc/server";
 import * as trpcNext from "@trpc/server/adapters/next";
 // export API handler
 export default trpcNext.createNextApiHandler({
   router: AppRouter,
   createContext: () => null,
 });
+
+export type inferQueryResponse<
+  TRoutekey extends keyof AppRouter["_def"]["queries"]
+> = inferProcedureOutput<AppRouter["_def"]["queries"][TRoutekey]>;
