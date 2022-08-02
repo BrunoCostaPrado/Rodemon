39aacff7c696ad9081e1b7209e96aaa9a8b2a039
BrunoCostaPrado <brunocostaprado@gmail.com>
2022-08-02 16:57:35 -0300

    Bugs fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,12 +10,19 @@ export default function Home() {
   const firstPokemon = trpc.useQuery(["get-pokemon-by-id", { id: first }]);
   const secondPokemon = trpc.useQuery(["get-pokemon-by-id", { id: second }]);
   if (firstPokemon.isLoading || secondPokemon.isLoading) return null;
+  const voteForRoundest = (selected: number) => {
+    updateIds(getOptionsForVote());
+  };
+  console.log(ids);
+  const btn =
+    "inline-flex items-center px-3 py-1.5 border border-gray-300 shadow-sm font-medium rounded-full text-gray-700 bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500";
+
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
       <div className="text-2xl text-center">Qual Pokémon é mais redondo?</div>
       <div className="p-2" />
       <div className="border rounded p-8 flex justify-between max-w-2xl items-center">
-        <div className="w-64 h-64 flex flex-col">
+        <div className="w-64 h-64 flex flex-col items-center">
           <img
             src={firstPokemon.data?.sprites.front_default}
             className="w-full"
@@ -23,9 +30,12 @@ export default function Home() {
           <div className="text-center text-xl capitalize mt-[-2rem]">
             {firstPokemon.data?.name}
           </div>
+          <button className={btn} onClick={() => voteForRoundest(first)}>
+            Mais Redondo
+          </button>
         </div>
         <div className="p-8">Vs</div>
-        <div className="w-64 h-64 flex flex-col">
+        <div className="w-64 h-64 flex flex-col items-center">
           <img
             src={secondPokemon.data?.sprites.front_default}
             className="w-full"
@@ -33,6 +43,9 @@ export default function Home() {
           <div className="text-center text-xl capitalize mt-[-2rem]">
             {secondPokemon.data?.name}
           </div>
+          <button className={btn} onClick={() => voteForRoundest(second)}>
+            Mais Redondo
+          </button>
         </div>
 
         <div className="p-2" />
